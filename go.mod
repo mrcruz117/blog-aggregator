72b5da80c6b369d99df2bc64e13afd9241a6bd1f
mrcruz117 <mrcruz1989@gmail.com>
2024-09-24 21:00:41 -0400

    init libraries added

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/mrcruz117/blog-aggregator
 
 go 1.22.3
+
+require github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -4,10 +4,14 @@ import (
 	"fmt"
 	"log"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	fmt.Println("Hello, World!")
+
+	godotenv.Load()
 
 	portString := os.Getenv("PORT")
 	if portString == "" {
