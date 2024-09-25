2f43cfbe30b7658cfc49447fd0f0914a631a0b92
mrcruz117 <mrcruz1989@gmail.com>
2024-09-24 20:52:43 -0400

    port init

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
+func main() {
+	fmt.Println("Hello, World!")
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT not found...")
+	}
+
+	fmt.Println("PORT:", portString)
 }
