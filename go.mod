8a1fa4668922bfd664e9e41eb77b7424e6ffa2a7
mrcruz117 <mrcruz1989@gmail.com>
2024-09-26 21:29:49 -0400

    config set up

--- go.mod
+++ go.mod
@@ -1,9 +1,3 @@
 module github.com/mrcruz117/blog-aggregator
 
 go 1.22.3
-
-require (
-	github.com/go-chi/chi v1.5.5
-	github.com/go-chi/cors v1.2.1
-	github.com/joho/godotenv v1.5.1
-)
--- main.go
+++ main.go
@@ -1,44 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"log"
-	"net/http"
-	"os"
 
-	"github.com/go-chi/chi"
-	"github.com/go-chi/cors"
-	"github.com/joho/godotenv"
+	"github.com/mrcruz117/blog-aggregator/internal/config"
 )
 
 func main() {
-
-	godotenv.Load()
-
-	portString := os.Getenv("PORT")
-	if portString == "" {
-		log.Fatal("PORT not found...")
-	}
-
-	router := chi.NewRouter()
-
-	router.Use(cors.Handler(cors.Options{
-		AllowedOrigins:   []string{"https://*", "http://*"},
-		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowedHeaders:   []string{"*"},
-		ExposedHeaders:   []string{"Link"},
-		AllowCredentials: false,
-		MaxAge:           300,
-	}))
-
-	srv := &http.Server{
-		Handler: router,
-		Addr:    ":" + portString,
-	}
-
-	log.Printf("Server is running on port %v", portString)
-	err := srv.ListenAndServe()
+	cfg, err := config.Read()
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("error reading config: %v", err)
+	}
+	fmt.Printf("Read config: %+v\n", cfg)
+
+	err = cfg.SetUser("michael")
+	if err != nil {
+		log.Fatalf("error setting user: %v", err)
 	}
 
+	cfg, err = config.Read()
+	if err != nil {
+		log.Fatalf("error reading config: %v", err)
+	}
+	fmt.Printf("Read config again: %+v\n", cfg)
 }
