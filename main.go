124784bd8b07007b626a74254075b078ac2dfe7b
mrcruz117 <mrcruz1989@gmail.com>
2024-09-27 17:42:01 -0400

    login command

--- main.go
+++ main.go
@@ -3,25 +3,44 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/mrcruz117/blog-aggregator/internal/config"
 )
+
+type state struct {
+	cfg *config.Config
+}
 
 func main() {
 	cfg, err := config.Read()
 	if err != nil {
 		log.Fatalf("error reading config: %v", err)
 	}
-	fmt.Printf("Read config: %+v\n", cfg)
 
-	err = cfg.SetUser("michael")
-	if err != nil {
-		log.Fatalf("error setting user: %v", err)
+	programState := &state{
+		cfg: &cfg,
 	}
 
-	cfg, err = config.Read()
-	if err != nil {
-		log.Fatalf("error reading config: %v", err)
+	cmds := commands{
+		registeredCommands: make(map[string]func(*state, command) error),
 	}
-	fmt.Printf("Read config again: %+v\n", cfg)
+
+	cmds.register("login", handlerLogin)
+
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: cli <command> [args...]")
+		os.Exit(1)
+	}
+
+	cmdName := os.Args[1]
+	cmdArgs := []string{}
+	if len(os.Args) > 2 {
+		cmdArgs = os.Args[2:]
+	}
+
+	err = cmds.run(programState, command{Name: cmdName, Args: cmdArgs})
+	if err != nil {
+		log.Fatal(err)
+	}
 }
